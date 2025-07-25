ec762a2ba7fb24c9945a58ba12ea45a1b761d9ca
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-07-25 20:23:09 +0200

    feat: Implement new business logic exceptions.

---
+++ backend/src/main/java/com/go_exchange_easier/backend/exception/NotExistingRoleException.java
@@ -0,0 +1,9 @@
+package com.go_exchange_easier.backend.exception;
+
+public class NotExistingRoleException extends RuntimeException {
+
+    public NotExistingRoleException(String message) {
+        super(message);
+    }
+
+}
