36a5f88ab7f3c2249452a28a599a58ec1fa77d99
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-07-29 14:12:51 +0200

    refactor: Change parameters in JwtTokenValidator validate method, change logic of method in implementation.

--- backend/src/main/java/com/go_exchange_easier/backend/service/JwtTokenValidator.java
+++ backend/src/main/java/com/go_exchange_easier/backend/service/JwtTokenValidator.java
@@ -1,9 +1,7 @@
 package com.go_exchange_easier.backend.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-
 public interface JwtTokenValidator {
 
-    boolean validate(String token, UserDetails userDetails);
+    boolean validate(String token);
 
 }
---
+++ backend/src/main/java/com/go_exchange_easier/backend/service/impl/JwtTokenValidatorImpl.java
@@ -0,0 +1,48 @@
+package com.go_exchange_easier.backend.service.impl;
+
+import com.go_exchange_easier.backend.service.JwtTokenValidator;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import io.jsonwebtoken.security.SignatureException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenValidatorImpl implements JwtTokenValidator {
+
+    private static final Logger logger = LogManager.getLogger(
+            JwtTokenValidatorImpl.class);
+    private final SecretKey signingKey;
+
+    @Override
+    public boolean validate(String token) {
+        try {
+            tryValidate(token);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token format: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty or malformed: {}", e.getMessage());
+        } catch (JwtException e) {
+            logger.error("An unexpected JWT error occurred: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    private void tryValidate(String token) {
+        Jwts.parser()
+                .verifyWith(signingKey)
+                .build()
+                .parseSignedClaims(token);
+    }
+
+}
