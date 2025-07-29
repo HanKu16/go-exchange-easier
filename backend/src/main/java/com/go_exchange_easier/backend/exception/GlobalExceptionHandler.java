0c1e6559e4020957ea1d4c0c4b3471ea9c68f1c1
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-07-29 14:33:49 +0200

    feat: Implement handling new Exception types, add new file to .gitignore.

--- backend/src/main/java/com/go_exchange_easier/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/go_exchange_easier/backend/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.security.SignatureException;
 import java.util.List;
 
 @ControllerAdvice
@@ -43,6 +44,36 @@ public class GlobalExceptionHandler {
                 "Authentication failed.");
         return new ResponseEntity<ErrorResponse>(
                 response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(NotExistingRoleException.class)
+    public ResponseEntity<ErrorResponse> handleNotExistingRoleException(
+            NotExistingRoleException e) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Server error.");
+        return new ResponseEntity<ErrorResponse>(
+                response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ErrorResponse> handleSignatureException(
+            SignatureException e) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(),
+                "Access denied.");
+        return new ResponseEntity<ErrorResponse>(
+                response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MissingJwtClaimException.class)
+    public ResponseEntity<ErrorResponse> handleMissingJwtClaimException(
+            MissingJwtClaimException e) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage());
+        return new ResponseEntity<ErrorResponse>(
+                response, HttpStatus.BAD_REQUEST);
     }
 
 }
