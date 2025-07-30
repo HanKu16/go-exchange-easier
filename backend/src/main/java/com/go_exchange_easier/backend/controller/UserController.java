5675a1933e23bb85ab3f212741793e796ecf485b
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-07-30 14:11:26 +0200

    feat: Add new endpoints abouts user.

---
+++ backend/src/main/java/com/go_exchange_easier/backend/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.go_exchange_easier.backend.controller;
+
+import com.go_exchange_easier.backend.dto.user.AssignHomeUniversityRequest;
+import com.go_exchange_easier.backend.dto.user.AssignHomeUniversityResponse;
+import com.go_exchange_easier.backend.dto.user.UpdateDescriptionRequest;
+import com.go_exchange_easier.backend.dto.user.UpdateUserDescriptionResponse;
+import com.go_exchange_easier.backend.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class UserController {
+
+    private final UserService userService;
+
+    @PatchMapping("/{userId}/description")
+    @PreAuthorize("#userId == authentication.principal.id")
+    public ResponseEntity<UpdateUserDescriptionResponse> updateDescription(
+            @PathVariable Integer userId,
+            @RequestBody @Valid UpdateDescriptionRequest request) {
+        UpdateUserDescriptionResponse response = userService
+                .updateDescription(userId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{userId}/homeUniversity")
+    @PreAuthorize("#userId == authentication.principal.id")
+    public ResponseEntity<AssignHomeUniversityResponse> assignHomeUniversity(
+            @PathVariable Integer userId,
+            @RequestBody @Valid AssignHomeUniversityRequest request) {
+        AssignHomeUniversityResponse response = userService.assignHomeUniversity(
+                userId, request);
+        return ResponseEntity.ok(response);
+    }
+
+}
