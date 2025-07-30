338c948cc8882824cab687f42afb824630e76445
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-07-30 17:00:58 +0200

    feat: Implement UniversityRepository, which should be included earlier.

---
+++ backend/src/main/java/com/go_exchange_easier/backend/repository/UniversityRepository.java
@@ -0,0 +1,6 @@
+package com.go_exchange_easier.backend.repository;
+
+import com.go_exchange_easier.backend.model.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityRepository extends JpaRepository<University, Short> { }
