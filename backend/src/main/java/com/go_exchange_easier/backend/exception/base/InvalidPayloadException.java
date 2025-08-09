0663f47045d36cbc6cb2c7cde05e170218c38064
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-08-09 12:54:20 +0200

    refactor: Rename InvalidPayloadException to ReferencedResourceNotFoundException. Delete exceptions about payload and replace them with ReferencedResourceNotFoundException.

--- backend/src/main/java/com/go_exchange_easier/backend/exception/base/InvalidPayloadException.java
+++
@@ -1,9 +0,0 @@
-package com.go_exchange_easier.backend.exception.base;
-
-public class InvalidPayloadException extends RuntimeException {
-
-    public InvalidPayloadException(String message) {
-        super(message);
-    }
-
-}
