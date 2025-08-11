e0ddd3aaa4e877e4c70a0530e1ecf6b71de65ca3
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-08-11 15:17:53 +0200

    Add open api dependency. Make doc available without authentiaction and authorization. Configure pom.xml to show generated sql. Add jwt authentication to to open api ui.

--- backend/src/main/java/com/go_exchange_easier/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/go_exchange_easier/backend/security/SecurityConfig.java
@@ -33,6 +33,8 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
