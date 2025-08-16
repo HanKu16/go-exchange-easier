cdcc7f5627bb23fc5b36e53847ec7c82cf425bda
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-08-16 12:07:04 +0200

    refactor: Refactor UserService methods to execute less queries to database. Add new methods to repositories.

--- backend/src/main/java/com/go_exchange_easier/backend/repository/UserRepository.java
+++ backend/src/main/java/com/go_exchange_easier/backend/repository/UserRepository.java
@@ -2,8 +2,22 @@ package com.go_exchange_easier.backend.repository;
 
 import com.go_exchange_easier.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends
-        JpaRepository<User, Integer> { }
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Modifying
+    @Query("UPDATE User u SET u.status.id = :statusId WHERE u.id = :userId")
+    int updateStatus(@Param("userId") int userId, @Param("statusId") short statusId);
+
+    @Modifying
+    @Query("UPDATE User u SET u.homeUniversity.id = :homeUniversityId " +
+            "WHERE u.id = :userId")
+    int updateHomeUniversity(@Param("userId") int userId,
+            @Param("homeUniversityId") short homeUniversityId);
+
+}
--- backend/src/main/java/com/go_exchange_easier/backend/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/go_exchange_easier/backend/service/impl/UserServiceImpl.java
@@ -4,8 +4,6 @@ import com.go_exchange_easier.backend.dto.user.*;
 import com.go_exchange_easier.backend.exception.base.ReferencedResourceNotFoundException;
 import com.go_exchange_easier.backend.exception.domain.UserNotFoundException;
 import com.go_exchange_easier.backend.model.University;
-import com.go_exchange_easier.backend.model.User;
-import com.go_exchange_easier.backend.model.UserDescription;
 import com.go_exchange_easier.backend.model.UserStatus;
 import com.go_exchange_easier.backend.repository.UniversityRepository;
 import com.go_exchange_easier.backend.repository.UserDescriptionRepository;
@@ -14,58 +12,64 @@ import com.go_exchange_easier.backend.repository.UserStatusRepository;
 import com.go_exchange_easier.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.time.OffsetDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
     private final UserDescriptionRepository userDescriptionRepository;
     private final UniversityRepository universityRepository;
     private final UserStatusRepository userStatusRepository;
+    private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UpdateUserDescriptionResponse updateDescription(
             int userId, UpdateUserDescriptionRequest request) {
-        UserDescription description = userDescriptionRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException(
-                        "Description for user of id " + userId + " was not found. " +
-                                "Probably user of given id does not exist."));
-        description.setTextContent(request.description());
-        description.setUpdatedAt(OffsetDateTime.now());
-        UserDescription updatedDescription = userDescriptionRepository.save(description);
+        OffsetDateTime updatedAt = OffsetDateTime.now();
+        int rowsUpdated = userDescriptionRepository.updateByUserId(
+                userId, request.description(), updatedAt);
+        if (rowsUpdated == 0) {
+            throw new UserNotFoundException("Description for user of id " +
+                    userId + " was not found.");
+        }
         return new UpdateUserDescriptionResponse(userId,
-                updatedDescription.getTextContent(),
-                updatedDescription.getUpdatedAt());
+                request.description(), updatedAt);
     }
 
     @Override
+    @Transactional
     public AssignHomeUniversityResponse assignHomeUniversity(
             int userId, AssignHomeUniversityRequest request) {
         University university = universityRepository.findById(request.universityId())
-                .orElseThrow(() -> new ReferencedResourceNotFoundException("University " +
-                        "of id " + request.universityId() + " does not exist."));
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException(
-                        "User of id " + userId + " does not exist."));
-        user.setHomeUniversity(university);
-        userRepository.save(user);
+                .orElseThrow(() -> new ReferencedResourceNotFoundException(
+                        "University of id " + request.universityId() +
+                                " does not exist."));
+        int rowsUpdated = userRepository.updateHomeUniversity(
+                userId, university.getId());
+        if (rowsUpdated == 0) {
+            throw new UserNotFoundException("User of id "
+                    + userId + " does not exist.");
+        }
         return new AssignHomeUniversityResponse(userId, university.getId(),
                 university.getOriginalName(), university.getEnglishName());
     }
 
     @Override
+    @Transactional
     public UpdateUserStatusResponse updateStatus(
             int userId, UpdateUserStatusRequest request) {
         UserStatus status = userStatusRepository.findById(request.statusId())
-                .orElseThrow(() -> new ReferencedResourceNotFoundException("Status " +
-                        "of id " + request.statusId() + " does not exist."));
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException(
-                        "User of id " + userId + " does not exist."));
-        user.setStatus(status);
-        userRepository.save(user);
+                .orElseThrow(() -> new ReferencedResourceNotFoundException(
+                        "Status of id " + request.statusId() +
+                                " was not found."));
+        int rowsUpdated = userRepository.updateStatus(userId, status.getId());
+        if (rowsUpdated == 0) {
+            throw new UserNotFoundException("User of id " + userId +
+                    " was not found.");
+        }
         return new UpdateUserStatusResponse(userId,
                 status.getId(), status.getName());
     }
