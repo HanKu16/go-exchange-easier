bd58b7e9bc166863f162dad4b8352240585d7d4f
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-01 21:20:27 +0200

    feat: Implement basic login page with request to backend add routes to App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login'
+import { Routes, Route } from "react-router-dom"
+import LoginPage from "./components/Login"
+import NotImplementedYet from "./components/NotImplementedYet"
 
 const App = () => {
   return (
     <Routes>
-      <Route path='login' element={<Login/>}/>
+      <Route path="login" element={ <LoginPage/> }/>
+      <Route path="register" element={ <NotImplementedYet/> }/>
+      <Route path="profile" element={ <NotImplementedYet/>}/>
+      <Route path="learn-more" element={ <NotImplementedYet/>}/>
+      <Route path="password-recovery" element={ <NotImplementedYet/>}/>
     </Routes>
   )
 }
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,44 @@
-import { useState } from "react"
+import type { LoginRequest } from "../dto/auth/LoginRequest";
+import type { LoginResult } from "../utils/auth";
+import { useEffect, useState } from "react"
+import { useNavigate, Link } from "react-router-dom";
+import { sendLoginRequest } from "../utils/auth";
 import FormInput from "./FormInput"
 
-const Login = () => {
-  const [login, setLogin] = useState("")
-  const [password, setPassword] = useState("")
+const LoginPage = () => {
+  const navigate = useNavigate()
+  const [login, setLogin] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [isLoginButtonClicked, setIsButtonClicked] = useState<boolean>(false)
+  const [loginFailedMessage, setLoginFailedMessage] = useState<string>("")
+  
+  const handleLogin = async () => {
+    const body: LoginRequest = {
+      login,
+      password
+    }
+    const result: LoginResult = await sendLoginRequest(body)
+    if (result.isSuccess) {
+        localStorage.setItem("jwtToken", result.data.accessToken)
+        localStorage.setItem("tokenType", result.data.tokenType)
+        navigate("/profile")
+    } else {
+        setLogin(() => "")
+        setPassword(() => "")
+        setIsButtonClicked(() => false)
+        setLoginFailedMessage("Invalid credentials")
+    }
+  }
+
+  useEffect(() => {
+    document.title = "Go Exchange Easier"
+  }, [])
+
+  useEffect(() => {
+    if (isLoginButtonClicked) {
+      handleLogin()
+    }
+  }, [isLoginButtonClicked])
 
   return (
     <div className="bg-dark-blue h-screen flex justify-center items-center">
@@ -26,30 +61,39 @@ const Login = () => {
             <FormInput label="Password" id="password-input" value={ password } 
               onChange={ event => setPassword(event.target.value) }
               placeholder="Enter your password"/>
-            <p className="text-[0.7vw] font-semibold !mt-1.5 !p-1">
+            <Link to="/password-recovery" className="text-[0.7vw] font-semibold 
+              !mt-1.5 !p-1 hover:text-shadow-lg hover:font-bold duration-100 ease-in">
               Forgot password?
-            </p>
+            </Link>
           </div>
           <div className="h-2/10 flex flex-col items-center">
             <button className="bg-ocean-blue w-19/20 rounded-xl shadow-lg text-white !py-2 !mb-2
               transition-colors duration-250 hover:bg-ocean-blue-dark
-              ease-in-out active:scale-99 active:shadow-sm">
+              ease-in-out active:scale-99 active:shadow-sm"
+              onClick={() => setIsButtonClicked(true)}>
               Sign In
             </button>
             <p className="text-[0.7vw]">
               Don't have an account?
-              <span className="text-ocean-blue"> Sign up for free!</span>
+              <Link to="/register" className="text-ocean-blue hover:font-semibold 
+                duration-150 ease-in"> 
+                Sign up for free!
+              </Link>
             </p>
           </div>
         </div>
-        <div className="h-1/5 w-full flex items-center justify-center">
-          <p className="text-amber-400 !pb-10 text-[0.9vw]">
-            Learn more about us here!
+        <div className="h-1/5 w-full flex flex-col items-center justify-around">
+          <p className="text-red-600 text-[1vw] font-semibold text-shadow-sm">
+            { loginFailedMessage }
           </p>
+          <Link to="/learn-more" className="text-amber-400 !pb-10 text-[0.9vw] 
+            hover:text-amber-600 duration-150 ease-in">
+            Learn more about us here!
+          </Link>
         </div>
       </div>
     </div>
   )
 }
 
-export default Login
+export default LoginPage
