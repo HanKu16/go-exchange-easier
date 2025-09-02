5ad747c04648c10351e23707a5e0eb723c8b87a8
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-02 13:18:37 +0200

    feat: Add icon and new routes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import LoginPage from "./components/LoginPage"
+import RegistrationPage from "./components/registration/RegistrationPage"
 import NotImplementedYet from "./components/NotImplementedYet"
 
 const App = () => {
   return (
     <Routes>
       <Route path="login" element={ <LoginPage/> }/>
-      <Route path="register" element={ <NotImplementedYet/> }/>
-      <Route path="profile" element={ <NotImplementedYet/>}/>
-      <Route path="learn-more" element={ <NotImplementedYet/>}/>
-      <Route path="password-recovery" element={ <NotImplementedYet/>}/>
+      <Route path="register" element={ <RegistrationPage/> }/>
+      <Route path="profile" element={ <NotImplementedYet/> }/>
+      <Route path="learn-more" element={ <NotImplementedYet/> }/>
+      <Route path="password-recovery" element={ <NotImplementedYet/> }/>
+      <Route path="terms-and-conditions" element={ <NotImplementedYet/> }/>
     </Routes>
   )
 }
