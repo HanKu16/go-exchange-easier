c9e8780393687d8d86b2aea962cde7e7383faba6
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-02 19:11:58 +0200

    feat: Implement dtos for user registration.

---
+++ frontend/src/dto/user/UserRegistrationRequest.tsx
@@ -0,0 +1,6 @@
+export interface UserRegistrationRequest {
+  login: string;
+  password: string;
+  mail: string;
+  nick: string;
+}
---
+++ frontend/src/dto/user/UserRegistrationResponse.tsx
@@ -0,0 +1,6 @@
+export interface UserRegistrationResponse {
+  userId: number;
+  login: string;
+  nick: string;
+  createdAt: string;
+}
