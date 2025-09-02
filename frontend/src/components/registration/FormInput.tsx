96d84a86859fa1d2507426a4f2d2ec82e23653fc
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-02 19:51:51 +0200

    refactor: Change components to use context to dynamically update their states.

--- frontend/src/components/registration/FormInput.tsx
+++ frontend/src/components/registration/FormInput.tsx
@@ -1,29 +1,32 @@
 import type { FormInputProps } from "../../props/registration/FormInputProps"
+import { useColorsContext } from "./SuggestionsColorsContext";
+import type { SuggestionsColors } from "./SuggestionsColorsContext";
 
 const FormInput = ( {label, id, value, isObligatory, 
   suggestions, onChange }: FormInputProps ) => {
+  const suggestionsColors: SuggestionsColors  = useColorsContext()
 
   return (
       <div className="w-7/10 flex justify-center items-center">
         <div className="flex flex-col w-9/10 !pl-10">
-          <label htmlFor={id} className="text-[1vw]">
+          <label htmlFor={ id } className="text-[1vw]">
             { label }
             <span className="text-yellow-500 font-bold text-[1.3vw]">
               { isObligatory ? "*" : "" }
             </span>
           </label>
           <input
-            id={id}
+            id={ id }
             type="text"
-            value={value}
-            onChange={onChange}
-            placeholder={`Enter your ${label.toLowerCase()}`}
+            value={ value }
+            onChange={ onChange }
+            placeholder={ `Enter your ${label.toLowerCase()}` }
             className="outline outline-neutral-300 focus:outline-neutral-400 
               focus:outline-2 rounded-lg shadow-md !px-1.5 !py-1 w-17/20 !my-2"
           />
-          {suggestions.map(s => (
-            <p key={s} className="text-zinc-500 text-[0.8vw]">{`-${ s }`}</p>
-          ))}
+        {suggestions.map(s => (
+          <p key={ s } className={`${suggestionsColors[id]} text-[0.8vw]`}>{`-${s}`}</p>
+        ))}
         </div>
       </div>
   )
--- frontend/src/components/registration/FormInputDescription.tsx
+++ frontend/src/components/registration/FormInputDescription.tsx
@@ -3,7 +3,7 @@ import type { FormInputDescriptionProps } from "../../props/registration/FormInp
 const FormInputDescription = ({ text }:  FormInputDescriptionProps ) => {
   return (
     <div className="flex justify-center items-center text-gray-400 w-3/10 text-center">
-      <p className="w-7/10 !pb-3 !pr-6">
+      <p className="w-7/10 !pr-6">
         { text }
       </p>
     </div>
