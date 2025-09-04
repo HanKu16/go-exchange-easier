cb035aa11595a9bbee04825173415501b4986e95
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-04 16:57:11 +0200

    feat: Change route name /profile to /user-profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import LoginPage from "./components/login/LoginPage"
 import RegistrationPage from "./components/registration/RegistrationPage"
 import NotImplementedYet from "./components/NotImplementedYet"
+import UserProfile from "./components/profile/user/UserProfile"
 
 const App = () => {
   return (
     <Routes>
       <Route path="login" element={ <LoginPage/> }/>
       <Route path="register" element={ <RegistrationPage/> }/>
-      <Route path="profile" element={ <NotImplementedYet/> }/>
+      <Route path="user-profile" element={ <UserProfile/> }/>
       <Route path="learn-more" element={ <NotImplementedYet/> }/>
       <Route path="password-recovery" element={ <NotImplementedYet/> }/>
       <Route path="terms-and-conditions" element={ <NotImplementedYet/> }/>
