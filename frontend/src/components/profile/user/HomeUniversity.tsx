abf47272871e03ff8a625381f4c367de7f9e697c
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-10 17:29:40 +0200

    refactor: Modify components to work with dynamic data form UserProfileContext.

--- frontend/src/components/profile/user/HomeUniversity.tsx
+++ frontend/src/components/profile/user/HomeUniversity.tsx
@@ -1,11 +1,18 @@
 import locationPin from "../../../assets/profile/user/location-pin.png"
+import { useUserProfileContext } from "./UserProfileContext"
 
 const HomeUniversity = () => {
+  const { user } = useUserProfileContext()
+  const universityLabel = user && user.homeUniversity ?
+    user.homeUniversity.nativeName + 
+    (user.homeUniversity.englishName ? `, ${user.homeUniversity.englishName}` : "") :
+    "no info about home university"
+
   return (
     <div className="xl:h-1/8 h-1/3 w-full flex xl:!pt-2 items-center !pr-3">
       <img src={ locationPin } className="h-3/4 aspect-1/1" />
-      <h2 className="font-semibold xl:text-[1.3vw] lg:text-[2vw] text-[3vw] !pl-3">
-        Università di Bologna, University of Bologna
+      <h2 className="font-semibold xl:text-[1.3vw] lg:text-[2vw] text-[3vw] !pl-3"> 
+        { universityLabel }
       </h2>
     </div>
   )
--- frontend/src/components/profile/user/Nick.tsx
+++ frontend/src/components/profile/user/Nick.tsx
@@ -1,16 +1,19 @@
-import flag from "../../../assets/profile/user/flag.png"
+import { useUserProfileContext } from "./UserProfileContext"
 
 const Nick = () => {
+  const { user } = useUserProfileContext()
+
   return (
     <div className="h-full w-full xl:w-2/3 flex items-center">
-      {/* <h1 className="text-lg 
-        xl:text-[2.5vw] font-semibold">
-          Kentucky Tusk
-      </h1> */}
       <span className="text-[5vw]
         lg:text-[3.5vw]
-        xl:text-[2.5vw] font-semibold">Kentucky Tusk</span>
-      <img src={ flag } className="h-[4vw] xl:h-[2vw] aspect-3/2 lg:!ml-3 !ml-2 !mb-1 xl:!mb-3" />
+        xl:text-[2.5vw] font-semibold">{ user?.nick }</span>
+        { user && user.countryOfOrigin ? (
+          <img src={`/flags/${ user.countryOfOrigin.name }.png`} 
+            className="h-[4vw] xl:h-[2vw] aspect-3/2 lg:!ml-3 
+            !ml-2 !mb-1 xl:!mb-3" />
+        ) : (<></>)
+        }
     </div>
   )
 }
