40268194f70517a4b09da7930b78c1974486f26d
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-19 14:53:05 +0200

    Implement sendGetUserProfileRequest and dto.

---
+++ frontend/src/dtos/user/GetUserProfileResponse.tsx
@@ -0,0 +1,19 @@
+export type GetUserProfileResponse = {
+  userId: number;
+  nick: string;
+  description: string;
+  isFollowed: boolean;
+  homeUniversity?: {
+    id: number;
+    nativeName: string;
+    englishName?: string;
+  };
+  countryOfOrigin?: {
+  id: number;
+  name: string;
+  };
+  status?: {
+    id: number;
+    name: string;
+  };
+}
--- frontend/src/utils/user.tsx
+++ frontend/src/utils/user.tsx
@@ -1,18 +1,33 @@
-import type { UserRegistrationRequest } from "../dtos/user/UserRegistrationRequest"
-import type { UserRegistrationResponse } from "../dtos/user/UserRegistrationResponse"
-import { sendRequest } from "./send-request";
-import type { ResponseSuccessResult } from "../types/ResonseSuccessResult";
-import type { RepsonseFailureResult } from "../types/ResponseFailureResult";
+import type { UserRegistrationRequest } from '../dtos/user/UserRegistrationRequest'
+import type { UserRegistrationResponse } from '../dtos/user/UserRegistrationResponse'
+import type { GetUserProfileResponse } from '../dtos/user/GetUserProfileResponse'
+import { sendRequest } from './send-request'
+import type { ResponseSuccessResult } from '../types/ResonseSuccessResult'
+import type { RepsonseFailureResult } from '../types/ResponseFailureResult'
 
 export const sendUserRegistrationRequest = async (body: UserRegistrationRequest): 
   Promise<ResponseSuccessResult<UserRegistrationResponse> | RepsonseFailureResult> => {
-  const uri: string = "http://localhost:8080/api/auth/register"
+  const uri: string = 'http://localhost:8080/api/auth/register'
   const request: RequestInit = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(body)
   }
   return await sendRequest(uri, request)
+}
+
+export const sendGetUserProfileRequest = async (userId: number | string) :
+  Promise<ResponseSuccessResult<GetUserProfileResponse> | RepsonseFailureResult> => {
+  const uri: string = `http://localhost:8080/api/users/${userId}/profile`
+  const jwtToken = localStorage.getItem("jwtToken")
+  const request: RequestInit = {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${jwtToken}`,
+      "Content-Type": "application/json",
+    }
+  }
+  return await sendRequest<GetUserProfileResponse>(uri, request)
 }
