a07061df4f4900209a2ca253df8cc5e952c7945e
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-21 17:39:47 +0200

    feat: Implement dto for assign country to user request.

---
+++ backend/src/main/java/com/go_exchange_easier/backend/dto/user/AssignCountryOfOriginResponse.java
@@ -0,0 +1,20 @@
+package com.go_exchange_easier.backend.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Response body for successful assigning country to user")
+public record AssignCountryOfOriginResponse(
+
+        Integer userId,
+        CountryDto country
+
+) {
+
+    public record CountryDto(
+
+            Short id,
+            String name
+
+    ) { }
+
+}
