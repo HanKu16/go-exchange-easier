d4967b1bb9f1dce51e2b9d26b1cfd22b09ab84a4
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-22 13:35:12 +0200

    feat: Implement get user exchanges feature.

--- backend/src/main/java/com/go_exchange_easier/backend/service/ExchangeService.java
+++ backend/src/main/java/com/go_exchange_easier/backend/service/ExchangeService.java
@@ -2,9 +2,12 @@ package com.go_exchange_easier.backend.service;
 
 import com.go_exchange_easier.backend.dto.exchange.CreateExchangeRequest;
 import com.go_exchange_easier.backend.dto.exchange.CreateExchangeResponse;
+import com.go_exchange_easier.backend.dto.exchange.GetUserExchangeResponse;
+import java.util.List;
 
 public interface ExchangeService {
 
+    List<GetUserExchangeResponse> getByUserId(int userId);
     CreateExchangeResponse create(int userId, CreateExchangeRequest request);
     void delete(int exchangeId);
 
