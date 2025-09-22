b8fe48a807fdf5a90f081f64e80284caeb11bd3d
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-22 14:46:20 +0200

    refactor: Refactor component and rename it.

--- frontend/src/components/NoContent.tsx
+++ frontend/src/components/NoContent.tsx
@@ -1,27 +1,25 @@
 import { Box, Typography } from '@mui/material'
 import RateReviewIcon from "@mui/icons-material/RateReview";
 
-export type NoReviewsFrameProps = {
-  isOwnProfile: boolean;
+export type NoContentProps = {
+  title: string;
+  subheader: string;
 }
 
-const NoReviewsFrame = (props: NoReviewsFrameProps) => {
+const NoContent = (props: NoContentProps) => {
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', 
       justifyContent: 'center', minHeight: 200, textAlign: 'center',
       color: 'text.secondary', p: 3}}>
       <RateReviewIcon sx={{ fontSize: 60, mb: 2, color: "grey.500" }} />
       <Typography variant="h6" gutterBottom>
-        No reviews yet
+        {props.title}
       </Typography>
       <Typography variant="body2">
-        {props.isOwnProfile ? 
-          "You haven't written any reviews." : 
-          "This user hasn't written any reviews."
-        }
+        {props.subheader}
       </Typography>
     </Box>
   );
 }
 
-export default NoReviewsFrame
+export default NoContent
