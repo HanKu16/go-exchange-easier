f55807041c68775383e4904e5d625d189289b950
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-23 21:45:01 +0200

    feat: Implement user requests methods.

--- frontend/src/utils/user.tsx
+++ frontend/src/utils/user.tsx
@@ -6,6 +6,14 @@ import type { ResponseSuccessResult } from '../types/ResonseSuccessResult'
 import type { RepsonseFailureResult } from '../types/ResponseFailureResult'
 import type { GetUniversityReviewResponse } from '../dtos/review/GetUniversityReviewResponse'
 import type { GetUserExchangeResponse } from '../dtos/user/GetUserExchangeResponse'
+import type { UpdateUserDescriptionRequest } from '../dtos/user/UpdateUserDescriptionRequest'
+import type { UpdateUserDescriptionResponse } from '../dtos/user/UpdateUserDescriptionResponse'
+import type { AssignHomeUniversityRequest } from '../dtos/user/AssignHomeUniversityRequest'
+import type { AssignHomeUniversityResponse } from '../dtos/user/AssignHomeUniversityResponse'
+import type { UpdateUserStatusRequest } from '../dtos/user/UpdateUserStatusRequest'
+import type { UpdateUserStatusResponse } from '../dtos/user/UpdateUserStatusResponse'
+import type { AssignCountryOfOriginRequest } from '../dtos/user/AssignCountryOfOriginRequest'
+import type { AssignCountryOfOriginResponse } from '../dtos/user/AssignCountryOfOriginResponse'
 
 export const sendUserRegistrationRequest = async (body: UserRegistrationRequest): 
   Promise<ResponseSuccessResult<UserRegistrationResponse> | RepsonseFailureResult> => {
@@ -23,7 +31,7 @@ export const sendUserRegistrationRequest = async (body: UserRegistrationRequest)
 export const sendGetUserProfileRequest = async (userId: number | string) :
   Promise<ResponseSuccessResult<GetUserProfileResponse> | RepsonseFailureResult> => {
   const uri: string = `http://localhost:8080/api/users/${userId}/profile`
-  const jwtToken = localStorage.getItem("jwtToken")
+  const jwtToken = localStorage.getItem('jwtToken')
   const request: RequestInit = {
     method: 'GET',
     headers: {
@@ -38,7 +46,7 @@ export const sendGetUserReviewsRequest = async (userId: number | string):
   Promise<ResponseSuccessResult<GetUniversityReviewResponse[]> | 
     RepsonseFailureResult> => {
   const uri: string = `http://localhost:8080/api/users/${userId}/universityReviews`
-  const jwtToken = localStorage.getItem("jwtToken")
+  const jwtToken = localStorage.getItem('jwtToken')
   const request: RequestInit = {
     method: 'GET',
     headers: {
@@ -53,7 +61,7 @@ export const sendGetUserExchangesRequest = async (userId: number | string):
   Promise<ResponseSuccessResult<GetUserExchangeResponse[]> | 
     RepsonseFailureResult> => {
   const uri: string = `http://localhost:8080/api/users/${userId}/exchanges`
-  const jwtToken = localStorage.getItem("jwtToken")
+  const jwtToken = localStorage.getItem('jwtToken')
   const request: RequestInit = {
     method: 'GET',
     headers: {
@@ -62,4 +70,68 @@ export const sendGetUserExchangesRequest = async (userId: number | string):
     }
   }
   return await sendRequest<GetUserExchangeResponse[]>(uri, request)
+}
+
+export const sendUpdateDescriptionRequest = async (body: UpdateUserDescriptionRequest):
+  Promise<ResponseSuccessResult<UpdateUserDescriptionResponse> | 
+    RepsonseFailureResult> => {
+  const uri: string = `http://localhost:8080/api/users/description`
+  const jwtToken = localStorage.getItem('jwtToken')
+  const request: RequestInit = {
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${jwtToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body)
+  }
+  return await sendRequest<UpdateUserDescriptionResponse>(uri, request)
+}
+
+export const sendAssignHomeUniversityRequest = async (body: AssignHomeUniversityRequest):
+  Promise<ResponseSuccessResult<AssignHomeUniversityResponse> | 
+    RepsonseFailureResult> => {
+  const uri: string = `http://localhost:8080/api/users/homeUniversity`
+  const jwtToken = localStorage.getItem('jwtToken')
+  const request: RequestInit = {
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${jwtToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body)
+  }
+  return await sendRequest<AssignHomeUniversityResponse>(uri, request)
+}
+
+export const sendUpdateStatusRequest = async (body: UpdateUserStatusRequest):
+  Promise<ResponseSuccessResult<UpdateUserStatusResponse> | 
+    RepsonseFailureResult> => {
+  const uri: string = `http://localhost:8080/api/users/status`
+  const jwtToken = localStorage.getItem('jwtToken')
+  const request: RequestInit = {
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${jwtToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body)
+  }
+  return await sendRequest<UpdateUserStatusResponse>(uri, request)
+}
+
+export const sendAssignCountryOfOriginRequest = async (body: AssignCountryOfOriginRequest):
+  Promise<ResponseSuccessResult<AssignCountryOfOriginResponse> | 
+    RepsonseFailureResult> => {
+  const uri: string = `http://localhost:8080/api/users/countryOfOrigin`
+  const jwtToken = localStorage.getItem('jwtToken')
+  const request: RequestInit = {
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${jwtToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body)
+  }
+  return await sendRequest<AssignCountryOfOriginResponse>(uri, request)
 }
