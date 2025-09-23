a1973db8bea361e7cb2f87f4c070387e99a76415
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-09-23 14:52:03 +0200

    feat: Implement dtos.

---
+++ backend/src/main/java/com/go_exchange_easier/backend/dto/country/GetCountryResponse.java
@@ -0,0 +1,8 @@
+package com.go_exchange_easier.backend.dto.country;
+
+public record GetCountryResponse(
+
+        Short id,
+        String englishName
+
+) { }
---
+++ backend/src/main/java/com/go_exchange_easier/backend/dto/userStatus/GetUserStatusResponse.java
@@ -0,0 +1,8 @@
+package com.go_exchange_easier.backend.dto.userStatus;
+
+public record GetUserStatusResponse(
+
+        Short id,
+        String name
+
+) { }
