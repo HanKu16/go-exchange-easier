b7edc065a9385af35ca4df58b594c53f1b04d33b
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-10-26 22:13:43 +0100

    refactor: Refactor UpdateUserStatusPanel to handle better error scenarios.

--- frontend/src/panels/UpdateUserStatusPanel.tsx
+++ frontend/src/panels/UpdateUserStatusPanel.tsx
@@ -4,19 +4,23 @@ import type { ReactElement } from 'react'
 import { Radio, RadioGroup } from '@mui/material'
 import { FormControl, Button } from '@mui/material'
 import { sendUpdateStatusRequest } from '../utils/user'
-import Alert from '@mui/material/Alert'
 import { sendGetUserStatusesRequest } from '../utils/user-status'
 import { FormControlLabel } from '@mui/material'
-import type { AlertMessage } from '../types/AlertMessage'
 import type { GetUserStatusResponse } from '../dtos/user-status/GetUserStatusResponse'
 import PanelHeader from '../components/PanelHeader'
+import { useSnackbar } from '../context/SnackBarContext'
+import type { DataFetchStatus } from '../types/DataFetchStatus'
+import LoadingContent from '../components/LoadingContent'
+import ContentLoadError from '../components/ContentLoadError'
 
 type UserStatus = GetUserStatusResponse
 
 const UpdateUserStatusPanel = () => {
   const [statuses, setStatuses] = useState<UserStatus[]>([])
   const [selectedStatusId, setSelectedStatusId] = useState<number | null>(null)
-  const [message, setMessage] = useState<AlertMessage | null>(null)
+  const [showConfirmButton, setShowConfirmButton] = useState<boolean>(false)
+  const { showAlert } = useSnackbar()
+  const [statuesFetchStatus, setStatuesFetchStatus] = useState<DataFetchStatus>('loading')
 
   const getStatuses = async () => {
     const result = await sendGetUserStatusesRequest()
@@ -24,6 +28,16 @@ const UpdateUserStatusPanel = () => {
       const allStatuses = result.data
       allStatuses.push({id: 0, name: 'UNKNOWN'})
       setStatuses(allStatuses)
+      setStatuesFetchStatus('success')
+    } else {
+      switch (result.error.status) {
+        case 'INTERNAL_SERVER_ERROR':
+          setStatuesFetchStatus('serverError')
+          break
+        case 'SERVICE_UNAVAILABLE':
+          setStatuesFetchStatus('connectionError')
+          break
+      }
     }
   }
 
@@ -32,22 +46,22 @@ const UpdateUserStatusPanel = () => {
   }, [])
 
   useEffect(() => {
-    setMessage(null)
+    setShowConfirmButton(true)
   }, [selectedStatusId])
 
   const handleStatusUpdate = async () => {
     const statusId = selectedStatusId !== 0 ? selectedStatusId : null
-    setMessage({type: 'info', content: 'Waiting for server response.'})
     const result = await sendUpdateStatusRequest({statusId: statusId})
     if (result.isSuccess) {
-      setMessage({type: 'success', content: 'Status was updated successfully.'})
+      showAlert('Status was updated successfully.', 'success')
+      setShowConfirmButton(false)
     } else {
-      setMessage({type: 'error', content: 'Failed to update status. Please try again later.'})
+      showAlert('Failed to update status. Please try again later.', 'error')
     }
   }
 
   const getInteractionElement = (statusId: number): ReactElement => {
-    if ((selectedStatusId === statusId) && (message === null)) {
+    if (showConfirmButton && (statusId === selectedStatusId)) {
       return (
         <Button variant='contained' size='small' sx={{marginLeft: 2}}
           onClick={handleStatusUpdate}>
@@ -58,30 +72,39 @@ const UpdateUserStatusPanel = () => {
     return <></>
   }
 
+  const getContent = () => {
+    switch (statuesFetchStatus) {
+      case 'success':
+        return (
+          <FormControl>
+            <RadioGroup name='statuses-buttons-group'
+              value={selectedStatusId}
+              onChange={(event) => {
+                setSelectedStatusId(Number(event.target.value))
+              }}>
+              {statuses.map(s => (
+                <FormControlLabel key={s.id} value={s.id} control={<Radio />} label={(
+                  <>
+                    {s.name}
+                    {getInteractionElement(s.id)}
+                  </>)}
+              />))}
+            </RadioGroup>
+          </FormControl>
+        )
+      case 'loading':
+        return <LoadingContent title='Loading statuses'/>  
+      case 'connectionError':
+        return <ContentLoadError title='Connection error' subheader='Failed to load statuses'/>
+      case 'serverError':
+        return <ContentLoadError title='Server error' subheader='Failed to load statuses'/>
+    }
+  }
+
   return (
     <Box>
       <PanelHeader label='Assign status that fits you the most'/>
-      {message != null ?
-        <Alert variant='filled' severity={message.type} sx={{marginY: 2}}>
-          {message.content}
-        </Alert> : 
-        <></>
-      }
-      <FormControl>
-        <RadioGroup name='statuses-buttons-group'
-          value={selectedStatusId}
-          onChange={(event) => {
-            setSelectedStatusId(Number(event.target.value))
-          }}>
-          {statuses.map(s => (
-            <FormControlLabel key={s.id} value={s.id} control={<Radio />} label={(
-              <>
-                {s.name}
-                {getInteractionElement(s.id)}
-              </>)}
-          />))}
-        </RadioGroup>
-      </FormControl>
+      {getContent()}
     </Box>
   )
 }
