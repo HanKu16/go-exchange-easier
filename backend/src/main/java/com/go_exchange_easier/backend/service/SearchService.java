fa1f8ae0ac0b613ca75f4564c1e68aff6789d1ed
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-11-09 22:19:39 +0100

    feat: Define and implement SearchService.

--- backend/src/main/java/com/go_exchange_easier/backend/service/SearchService.java
+++ backend/src/main/java/com/go_exchange_easier/backend/service/SearchService.java
@@ -1,13 +1,22 @@
 package com.go_exchange_easier.backend.service;
 
-import com.go_exchange_easier.backend.dto.search.GetUsersByExchangeResponse;
+import com.go_exchange_easier.backend.dto.search.GetUniversityResponse;
+import com.go_exchange_easier.backend.dto.search.GetUserByNickResponse;
+import com.go_exchange_easier.backend.dto.search.GetUserByExchangeResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import java.time.LocalDate;
 
 public interface SearchService {
 
-    Page<GetUsersByExchangeResponse> findUsersByExchange(
+    Page<GetUserByExchangeResponse> findUsersByExchange(
             Short universityId, Integer cityId, Short countryId,
-            Short majorId, Pageable pageable);
+            Short majorId, LocalDate startDate, LocalDate endDate,
+            Pageable pageable);
+    Page<GetUserByNickResponse> findUsersByNick(
+            String nick, Pageable pageable);
+    Page<GetUniversityResponse> findUniversities(
+            String nativeName, String englishName,
+            Integer cityId, Short countryId, Pageable pageable);
 
 }
--- backend/src/main/java/com/go_exchange_easier/backend/service/impl/SearchServiceImpl.java
+++ backend/src/main/java/com/go_exchange_easier/backend/service/impl/SearchServiceImpl.java
@@ -1,11 +1,17 @@
 package com.go_exchange_easier.backend.service.impl;
 
-import com.go_exchange_easier.backend.dto.search.GetUsersByExchangeResponse;
+import com.go_exchange_easier.backend.dto.search.GetUniversityResponse;
+import com.go_exchange_easier.backend.dto.search.GetUserByNickResponse;
+import com.go_exchange_easier.backend.dto.search.GetUserByExchangeResponse;
 import com.go_exchange_easier.backend.exception.domain.BadNumberOfSearchFiltersException;
 import com.go_exchange_easier.backend.model.Exchange;
+import com.go_exchange_easier.backend.model.University;
+import com.go_exchange_easier.backend.model.User;
 import com.go_exchange_easier.backend.repository.ExchangeRepository;
+import com.go_exchange_easier.backend.repository.UniversityRepository;
 import com.go_exchange_easier.backend.repository.UserRepository;
 import com.go_exchange_easier.backend.repository.specification.ExchangeSpecification;
+import com.go_exchange_easier.backend.repository.specification.UniversitySpecification;
 import com.go_exchange_easier.backend.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,6 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -21,32 +28,61 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class SearchServiceImpl implements SearchService {
 
+    private final UniversityRepository universityRepository;
     private final ExchangeRepository exchangeRepository;
     private final UserRepository userRepository;
 
 
     @Override
     @Transactional(readOnly = true)
-    public Page<GetUsersByExchangeResponse> findUsersByExchange(
+    public Page<GetUserByExchangeResponse> findUsersByExchange(
             Short universityId, Integer cityId, Short countryId,
-            Short majorId, Pageable pageable) {
+            Short majorId, LocalDate startDate, LocalDate endDate,
+            Pageable pageable) {
         int numberOfFiltersApplied = countMainFiltersApplied(Arrays.asList(
                 universityId, cityId, countryId));
         if (numberOfFiltersApplied != 1) {
             throw new BadNumberOfSearchFiltersException(
-                    "Only 1 filter can be applied, but " +
-                            numberOfFiltersApplied + " were applied.");
+                    "Only 1 filter among university, city and country " +
+                            "can be applied, but " + numberOfFiltersApplied +
+                            " were applied.");
         }
-        Specification<Exchange> specification = buildSpecification(
-                universityId, cityId, countryId, majorId);
+        Specification<Exchange> specification = buildSpecificationForFindByExchange(
+                universityId, cityId, countryId, majorId,
+                startDate, endDate);
         Page<Exchange> exchanges = exchangeRepository
                 .findAll(specification, pageable);
         return exchanges.map(this::mapUserWithExchangeToResponse);
     }
 
-    private Specification<Exchange> buildSpecification(
+    @Override
+    @Transactional(readOnly = true)
+    public Page<GetUserByNickResponse> findUsersByNick(String nick, Pageable pageable) {
+        Page<User> users = userRepository.findByNick(nick, pageable);
+        return users.map(this::mapUserToResponse);
+    }
+
+    @Override
+    @Transactional(readOnly = true)x
+    public Page<GetUniversityResponse> findUniversities(
+            String nativeName, String englishName,
+            Integer cityId, Short countryId,
+            Pageable pageable) {
+        if ((cityId != null) && (countryId != null)) {
+            throw new BadNumberOfSearchFiltersException(
+                    "Only 1 filter among country and city can be applied, " +
+                            "but both were applied.");
+        }
+        Specification<University> specification = buildSpecificationForFindUniversities(
+                nativeName, englishName, cityId, countryId);
+        Page<University> universities = universityRepository.findAll(specification, pageable);
+        return universities.map(this::mapUniversityToResponse);
+    }
+
+    private Specification<Exchange> buildSpecificationForFindByExchange(
             Short universityId, Integer cityId,
-            Short countryId, Short majorId) {
+            Short countryId, Short majorId,
+            LocalDate startDate, LocalDate endDate) {
         Specification<Exchange> specification = ExchangeSpecification.fetchUser();
         if (universityId != null) {
             specification = specification.and(ExchangeSpecification
@@ -62,31 +98,86 @@ public class SearchServiceImpl implements SearchService {
             specification = specification.and(ExchangeSpecification
                     .hasMajorId(majorId));
         }
+        if (startDate != null) {
+            specification = specification.and(ExchangeSpecification
+                    .hasStartDateAtLeast(startDate));
+        }
+        if (endDate != null) {
+            specification = specification.and(ExchangeSpecification
+                    .hasStartDateAtMost(endDate));
+        }
         return specification;
     }
 
-    private GetUsersByExchangeResponse mapUserWithExchangeToResponse(Exchange exchange) {
-        return new GetUsersByExchangeResponse(exchange.getUser().getId(),
+    private Specification<University> buildSpecificationForFindUniversities(
+            String nativeName, String englishName, Integer cityId, Short countryId) {
+        Specification<University> specification = (root, query, cb) -> null;
+        if (nativeName != null) {
+            specification = specification.and(UniversitySpecification
+                    .hasOriginalName(nativeName));
+        }
+        if (englishName != null) {
+            specification = specification.and(UniversitySpecification
+                    .hasEnglishName(englishName));
+        }
+        if (cityId != null) {
+            specification = specification.and(UniversitySpecification
+                    .hasCityId(cityId));
+        }
+        if (countryId != null) {
+            specification = specification.and(UniversitySpecification
+                    .hasCountryId(countryId));
+        }
+        return specification;
+    }
+
+    private GetUserByExchangeResponse mapUserWithExchangeToResponse(Exchange exchange) {
+        return new GetUserByExchangeResponse(exchange.getUser().getId(),
                 exchange.getUser().getNick(),
-                new GetUsersByExchangeResponse.ExchangeDto(exchange.getId(),
-                        new GetUsersByExchangeResponse.TimeRangeDto(
+                new GetUserByExchangeResponse.ExchangeDto(exchange.getId(),
+                        new GetUserByExchangeResponse.TimeRangeDto(
                                 exchange.getStartedAt(), exchange.getEndAt()),
-                        new GetUsersByExchangeResponse.UniversityDto(
+                        new GetUserByExchangeResponse.UniversityDto(
                                 exchange.getUniversity().getId(),
                                 exchange.getUniversity().getOriginalName(),
                                 exchange.getUniversity().getEnglishName(),
-                                new GetUsersByExchangeResponse.CityDto(
+                                new GetUserByExchangeResponse.CityDto(
                                         exchange.getUniversity().getCity().getId(),
                                         exchange.getUniversity().getCity().getEnglishName(),
-                                        new GetUsersByExchangeResponse.CountryDto(
+                                        new GetUserByExchangeResponse.CountryDto(
                                                 exchange.getUniversity().getCity()
                                                         .getCountry().getId(),
                                                 exchange.getUniversity().getCity()
                                                         .getCountry().getEnglishName()
                                         )
-                                )), new GetUsersByExchangeResponse.UniversityMajorDto(
+                                )), new GetUserByExchangeResponse.UniversityMajorDto(
                         exchange.getUniversityMajor().getId(), exchange.getUniversityMajor()
                         .getName())));
+    }
+
+    private GetUserByNickResponse mapUserToResponse(User user) {
+        return new GetUserByNickResponse(user.getId(), user.getNick(),
+                user.getCountryOfOrigin() != null ?
+                        new GetUserByNickResponse.CountryDto(
+                                user.getCountryOfOrigin().getId(),
+                                user.getCountryOfOrigin().getEnglishName()) :
+                        null,
+                user.getHomeUniversity() != null ?
+                        new GetUserByNickResponse.UniversityDto(
+                                user.getHomeUniversity().getId(),
+                                user.getHomeUniversity().getOriginalName(),
+                                user.getHomeUniversity().getEnglishName()) :
+                        null);
+    }
+
+    private GetUniversityResponse mapUniversityToResponse(University university) {
+        return new GetUniversityResponse(university.getId(),
+                university.getOriginalName(), university.getEnglishName(),
+                new GetUniversityResponse.CityDto(
+                university.getCity().getId(), university.getEnglishName(),
+                new GetUniversityResponse.CountryDto(
+                        university.getCity().getCountry().getId(),
+                        university.getCity().getCountry().getEnglishName())));
     }
 
     private int countMainFiltersApplied(List<Object> filters) {
