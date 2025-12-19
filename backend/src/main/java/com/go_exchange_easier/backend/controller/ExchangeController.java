b1ecbe88623379cece10e507923233a266481a2a
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-12-19 09:18:28 +0100

    feat: Create new endpoint with documentation.

--- backend/src/main/java/com/go_exchange_easier/backend/controller/ExchangeController.java
+++ backend/src/main/java/com/go_exchange_easier/backend/controller/ExchangeController.java
@@ -2,18 +2,26 @@ package com.go_exchange_easier.backend.controller;
 
 import com.go_exchange_easier.backend.annoations.docs.exchange.CreateExchangeApiDocs;
 import com.go_exchange_easier.backend.annoations.docs.exchange.DeleteExchangeApiDocs;
+import com.go_exchange_easier.backend.annoations.docs.exchange.GetPageApiDocs;
+import com.go_exchange_easier.backend.dto.details.ExchangeDetails;
 import com.go_exchange_easier.backend.dto.exchange.CreateExchangeRequest;
 import com.go_exchange_easier.backend.dto.exchange.CreateExchangeResponse;
 import com.go_exchange_easier.backend.model.UserCredentials;
+import com.go_exchange_easier.backend.dto.filter.ExchangeFilters;
 import com.go_exchange_easier.backend.service.ExchangeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/exchange")
@@ -23,6 +31,17 @@ import java.net.URI;
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
+
+    @GetMapping
+    @GetPageApiDocs
+    public ResponseEntity<Page<ExchangeDetails>> getPage(
+            @ParameterObject @ModelAttribute ExchangeFilters filters,
+            @ParameterObject Pageable pageable) {
+        Page<ExchangeDetails> page = exchangeService.getPage(filters, pageable);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                .body(page);
+    }
 
     @PostMapping
     @CreateExchangeApiDocs
