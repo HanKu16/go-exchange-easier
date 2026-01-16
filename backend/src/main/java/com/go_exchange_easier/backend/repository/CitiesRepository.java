f7601c98f7be96596a80e8eb53d12ca0b1fc8edb
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2026-01-16 19:04:14 +0100

    refactor: Reduce number of queries to 1 from 2.

--- backend/src/main/java/com/go_exchange_easier/backend/repository/CitiesRepository.java
+++ backend/src/main/java/com/go_exchange_easier/backend/repository/CitiesRepository.java
@@ -1,11 +1,20 @@
 package com.go_exchange_easier.backend.repository;
 
 import com.go_exchange_easier.backend.model.City;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface CitiesRepository extends
-        JpaRepository<City, Integer>,
-        JpaSpecificationExecutor<City> { }
+public interface CitiesRepository extends JpaRepository<City, Integer>,
+        JpaSpecificationExecutor<City> {
+
+    @Override
+    @EntityGraph(attributePaths = {"country"})
+    List<City> findAll(@Nullable Specification<City> specification);
+
+}
