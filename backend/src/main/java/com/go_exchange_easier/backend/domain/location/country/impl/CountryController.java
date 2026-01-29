21f39903c82efe9ffc695175418f36658bf1b3c3
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2026-01-30 00:21:11 +0100

    refactor: Delete unsused annotation and imports.

--- backend/src/main/java/com/go_exchange_easier/backend/domain/location/country/impl/CountryController.java
+++ backend/src/main/java/com/go_exchange_easier/backend/domain/location/country/impl/CountryController.java
@@ -1,6 +1,5 @@
 package com.go_exchange_easier.backend.domain.location.country.impl;
 
-import com.go_exchange_easier.backend.domain.location.annotations.country.GetAllApiDocs;
 import com.go_exchange_easier.backend.common.dto.Listing;
 import com.go_exchange_easier.backend.domain.location.country.CountryApi;
 import com.go_exchange_easier.backend.domain.location.country.CountryDetails;
@@ -8,7 +7,6 @@ import com.go_exchange_easier.backend.domain.location.country.CountryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -19,8 +17,6 @@ public class CountryController implements CountryApi {
 
     private final CountryService countryService;
 
-    @GetMapping
-    @GetAllApiDocs
     @Override
     public ResponseEntity<Listing<CountryDetails>> getAll() {
         List<CountryDetails> countries = countryService.getAll();
