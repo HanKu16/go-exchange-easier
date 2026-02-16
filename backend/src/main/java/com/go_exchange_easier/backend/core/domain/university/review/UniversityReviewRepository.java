81a745d1110851723a0f10cbadbfdadad4701531
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2026-02-16 17:24:26 +0100

    refactor: Change native sql queries methods to be faster by replacing big group by operation with lateral join.

--- backend/src/main/java/com/go_exchange_easier/backend/core/domain/university/review/UniversityReviewRepository.java
+++ backend/src/main/java/com/go_exchange_easier/backend/core/domain/university/review/UniversityReviewRepository.java
@@ -22,16 +22,7 @@ public interface UniversityReviewRepository extends
             ur.star_rating,
             ur.text_content,
             ur.created_at,
-            COALESCE(
-                json_agg(
-                    json_build_object(
-                        'type', rc.reaction_type,
-                        'count', rc.count,
-                        'isSet',  COALESCE((rc.reaction_type = urr.reaction_type), FALSE)
-                    )
-                ) FILTER (WHERE rc.reaction_type IS NOT NULL),
-                '[]'
-            ) AS reactions,
+            r.reactions,
             us.avatar_key,
             co.country_id,
             co.english_name,
@@ -41,44 +32,46 @@ public interface UniversityReviewRepository extends
         FROM core.university_reviews ur
         LEFT JOIN core.users us ON us.user_id = ur.author_id
         LEFT JOIN core.universities un ON un.university_id = ur.university_id
-        LEFT JOIN core.university_reviews_reaction_counts rc ON rc.university_review_id = ur.university_review_id
-        LEFT JOIN core.university_review_reactions urr ON
-            urr.university_review_id = ur.university_review_id AND
-            urr.author_id = :currentUserId
         LEFT JOIN core.cities ci ON ci.city_id = un.city_id
         LEFT JOIN core.countries co ON co.country_id = ci.country_id
-        WHERE us.user_id = :authorId AND us.deleted_at IS NULL AND
-            un.deleted_at IS NULL AND ur.deleted_at IS NULL
-        GROUP BY ur.university_review_id, us.user_id, us.nick,
-            un.university_id, un.english_name, un.original_name,
-            ur.star_rating, ur.text_content, ur.created_at,
-        	co.country_id, co.english_name, co.flag_key,
-        	ci.city_id, ci.english_name
-       """, nativeQuery = true)
+        LEFT JOIN LATERAL (
+            SELECT
+                COALESCE(
+                    json_agg(
+                        json_build_object(
+                            'type', rc.reaction_type,
+                            'count', rc.count,
+                            'isSet', (urr.author_id IS NOT NULL)
+                        )
+                    ),
+                    '[]'::json
+                ) AS reactions
+            FROM core.university_reviews_reaction_counts rc
+            LEFT JOIN core.university_review_reactions urr ON
+                urr.university_review_id = rc.university_review_id
+                AND urr.reaction_type = rc.reaction_type
+                AND urr.author_id = :currentUserId
+            WHERE rc.university_review_id = ur.university_review_id
+            ) r ON true
+        WHERE us.user_id = :authorId AND us.deleted_at IS NULL
+        AND un.deleted_at IS NULL AND ur.deleted_at IS NULL
+        ORDER BY ur.created_at DESC
+    """, nativeQuery = true)
     List<Object[]> findByAuthorId(@Param("authorId") int authorId,
             @Param("currentUserId") int currentUserId);
 
     @Query(value = """
         SELECT
             ur.university_review_id,
-           	us.user_id,
-           	us.nick,
-           	un.university_id,
+            us.user_id,
+            us.nick,
+            un.university_id,
             un.english_name,
             un.original_name,
             ur.star_rating,
             ur.text_content,
             ur.created_at,
-            COALESCE(
-                json_agg(
-                   	json_build_object(
-                        'type', rc.reaction_type,
-                        'count', rc.count,
-                        'isSet',  COALESCE((rc.reaction_type = urr.reaction_type), FALSE)
-                    )
-                ) FILTER (WHERE rc.reaction_type IS NOT NULL),
-                '[]'
-            ) AS reactions,
+            r.reactions,
             us.avatar_key,
             co.country_id,
             co.english_name,
@@ -88,21 +81,31 @@ public interface UniversityReviewRepository extends
         FROM core.university_reviews ur
         LEFT JOIN core.users us ON us.user_id = ur.author_id
         LEFT JOIN core.universities un ON un.university_id = ur.university_id
-        LEFT JOIN core.university_reviews_reaction_counts rc ON
-            rc.university_review_id = ur.university_review_id
-        LEFT JOIN core.university_review_reactions urr ON
-           	urr.university_review_id = ur.university_review_id AND
-            urr.author_id = :currentUserId
         LEFT JOIN core.cities ci ON ci.city_id = un.city_id
         LEFT JOIN core.countries co ON co.country_id = ci.country_id
-        WHERE un.university_id = :universityId AND
-            un.deleted_at IS NULL AND us.deleted_at IS NULL AND
-            ur.deleted_at IS NULL
-        GROUP BY ur.university_review_id, us.user_id, us.nick,
-           	un.university_id, un.english_name, un.original_name,
-            ur.star_rating, ur.text_content, ur.created_at,
-            co.country_id, co.english_name, co.flag_key,
-        	ci.city_id, ci.english_name
+        LEFT JOIN LATERAL (
+            SELECT
+                COALESCE(
+                    json_agg(
+                        json_build_object(
+                            'type', rc.reaction_type,
+                            'count', rc.count,
+                            'isSet', (urr.author_id IS NOT NULL)
+                        )
+                    ),
+                    '[]'::json
+                ) AS reactions
+            FROM core.university_reviews_reaction_counts rc
+        LEFT JOIN core.university_review_reactions urr ON
+        urr.university_review_id = rc.university_review_id AND
+        urr.reaction_type = rc.reaction_type AND
+        urr.author_id = :currentUserId
+        WHERE rc.university_review_id = ur.university_review_id
+        ) r ON true
+        WHERE un.university_id = :universityId
+        AND un.deleted_at IS NULL
+        AND us.deleted_at IS NULL
+        AND ur.deleted_at IS NULL
         ORDER BY ur.created_at DESC
         LIMIT :limit
         OFFSET :offset;
